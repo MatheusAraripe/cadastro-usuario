07e0619352840e845950acbcea748f063678f1c0
Matheus Araripe <matheus.alc2001@gmail.com>
2023-07-20 13:32:00 -0300

    feat: adicionando responsividade ao card de contato

--- src/components/contactCard/ContactCard.jsx
+++ src/components/contactCard/ContactCard.jsx
@@ -5,12 +5,12 @@ import React from 'react'
 function ContactCard() {
   return (
     <Box className='cardContainer' px={5} py={2} >
-        <Box py={2} display={'flex'} justifyContent={'space-between'} alignItems={'center'}>
+        <Box py={2} display={'flex'} justifyContent={'space-between'} alignItems={'center'} className="cardHead">
             <h2>Matheus Araripe Lopes Correa</h2>
             <p>Nascimento: 02/06/2001</p>
         </Box>
-        <Box display={"flex"} justifyContent={'start'} alignItems={'center'} className="mainInfo" >
-            <Avatar sx={{marginRight: "3rem"}}>M</Avatar>
+        <Box className="mainInfo" >
+            <Avatar className='avatar-card'>M</Avatar>
             <p>CPF: 183.021.467.57</p>
             <p>Sexo: Masculino</p>
             <p>Endereço: Av Oswaldo Cruz 139</p>
--- src/components/mainContainer/MainContainer.jsx
+++ src/components/mainContainer/MainContainer.jsx
@@ -7,13 +7,13 @@ function MainContainer({ children, title }) {
     <Container sx={{height: "100vh"}} maxWidth={'100vh'}>
         <Grid container>
             {/* esconde a navbar para mobile */}
-            <Hidden smDown>
+            <Hidden mdDown>
                 <Grid item md={2} sm={2}>
                     <Navbar />
                 </Grid>
             </Hidden>
-            <Grid item md={8} sm={4}>
-                <Box my={15}>
+            <Grid item md={8} sm={12} xs={12}>
+                <Box my={15} >
                     <h1>{title}</h1>
                     {children}
                 </Box>
