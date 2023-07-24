a5b2567e3ac3d247c1ba07ecfeb4d195ef8b6387
Matheus Araripe <matheus.alc2001@gmail.com>
2023-07-24 17:17:54 -0300

    feat: adição das validações no formulário

--- src/components/form/ContactForm.jsx
+++ src/components/form/ContactForm.jsx
@@ -1,4 +1,6 @@
-import { TextField, Radio, RadioGroup, Grid, FormControlLabel, FormControl, FormLabel, Button} from '@mui/material'
+import { TextField, Radio, RadioGroup, Grid, FormControlLabel, FormControl, FormLabel, Button, FormHelperText} from '@mui/material'
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import { makeStyles } from '@mui/styles';
 import { DatePicker } from '@mui/x-date-pickers';
 import MaleIcon from '@mui/icons-material/Male';
@@ -14,47 +16,115 @@ const useStyles = makeStyles({
 });
 
 function ContactForm() {
+
+  const validationSchema = yup.object().shape({
+        name: yup.string().required('O nome é obrigatório'),
+        cpf: yup.string().required('O CPF é obrigatório').min(11, 'CPF incompleto').max(11,'Formato inválido'),
+        address: yup.string().required('Endereço é obrigatório'),
+        date: yup.date().required('Data obrigatória'),
+    });
+
+
+  // Configurar o useFormik com a resolução do Yup
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      cpf: '',
+      address: '',
+      date: '',
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      // Lógica para lidar com o envio do formulário
+      console.log(values);
+    },
+  });
   
   const classes = useStyles()
   return (
-    <FormControl fullWidth>
-        <Grid container bgcolor={'purple.main'} rowSpacing={4} color='purple.text' p={2} justifyContent={'center'}>
-            <h2>Adicione um novo contato</h2>
-            <Grid xs={12} sm={12} item>
-                <TextField id="name" label="Nome completo" variant="outlined" fullWidth color="secondary"/>
+    <form onSubmit={formik.handleSubmit}>
+            <Grid container rowSpacing={4} color='purple.text' p={2} justifyContent={'center'}>
+                <h2>Adicione um novo contato</h2>
+                <Grid xs={12} sm={12} item>
+                    <FormControl fullWidth error={formik.touched.name && Boolean(formik.errors.name)}>
+                        <TextField 
+                        name='name' 
+                        label='Nome completo' 
+                        variant="outlined" 
+                        fullWidth 
+                        color="secondary"
+                        value={formik.values.name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        />
+                        {formik.touched.name && <FormHelperText>{formik.errors.name}</FormHelperText>}
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                    <FormControl fullWidth error={formik.touched.cpf && Boolean(formik.errors.cpf)}>
+                        <TextField 
+                        name="cpf" 
+                        label="CPF" 
+                        variant="outlined" 
+                        fullWidth 
+                        color="secondary"
+                        value={formik.values.cpf}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        />
+                        {formik.touched.cpf && <FormHelperText>{formik.errors.cpf}</FormHelperText>}
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12}  >
+                    <FormControl fullWidth error={formik.touched.cpf && Boolean(formik.errors.cpf)}>
+                        <DatePicker
+                        value={formik.values.date}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        label="Data de nascimento" 
+                        sx={{width: '100%'}}
+                        />
+                        {/* {formik.touched.date && <FormHelperText>{formik.errors.date}</FormHelperText>} */}
+                    </FormControl>
+                </Grid>
+                <Grid xs={12}  item display={'flex'}  justifyContent={'start'} alignItems={'center'}>
+                    <FormLabel id="genderLable" sx={{color: 'purple.text', marginRight: '3px'}} color='secondary'>Sexo</FormLabel>
+                    <RadioGroup
+                    defaultValue="female"
+                    fullWidth
+                    name="genderOptions"
+                    row
+                    >
+                        <FormControlLabel 
+                        value="female"
+                        control={<Radio disableRipple icon={<FemaleIcon sx={{color: 'purple.text'}}/>} checkedIcon={<FemaleIcon sx={{color: 'purple.dark'}}/>}/>}
+                        label="Feminino" />
+                        <FormControlLabel 
+                        value="male" 
+                        control={<Radio disableRipple icon={<MaleIcon sx={{color: 'purple.text'}}/>} checkedIcon={<MaleIcon sx={{color: 'purple.dark'}}/>}/>} 
+                        label="Masculino" />
+                    </RadioGroup>
+                </Grid>
+                <Grid item xs={12} sm={12}>
+                    <FormControl fullWidth error={formik.touched.address && Boolean(formik.errors.address)}>
+                        <TextField 
+                        name='address' 
+                        label='Endereço' 
+                        variant='outlined' 
+                        fullWidth 
+                        color='secondary'
+                        value={formik.values.address}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        />
+                        {formik.touched.address && <FormHelperText>{formik.errors.address}</FormHelperText>}
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="text" type="submit" fullWidth className={classes.btn} sx={{ color: 'purple.text'}}>Criar contato</Button>
+                </Grid>
             </Grid>
-            <Grid item xs={12}  >
-                <TextField id="cpf" label="CPF" variant="outlined" fullWidth color="secondary" />
-            </Grid>
-            <Grid item xs={12}  >
-                <DatePicker label="Data de nascimento" sx={{width: '100%'}}/>
-            </Grid>
-            <Grid xs={12}  item display={'flex'}  justifyContent={'start'} alignItems={'center'}>
-                <FormLabel id="genderLable" sx={{color: 'purple.text', marginRight: '3px'}} color='secondary'>Sexo</FormLabel>
-                <RadioGroup
-                defaultValue="female"
-                fullWidth
-                name="genderOptions"
-                row
-                >
-                    <FormControlLabel 
-                    value="female"
-                    control={<Radio disableRipple icon={<FemaleIcon sx={{color: 'purple.text'}}/>} checkedIcon={<FemaleIcon sx={{color: 'purple.dark'}}/>}/>}
-                    label="Feminino" />
-                    <FormControlLabel 
-                    value="male" 
-                    control={<Radio disableRipple icon={<MaleIcon sx={{color: 'purple.text'}}/>} checkedIcon={<MaleIcon sx={{color: 'purple.dark'}}/>}/>} 
-                    label="Masculino" />
-                </RadioGroup>
-            </Grid>
-            <Grid item xs={12} sm={12}>
-                <TextField id='adresses' label='Endereço' variant='outlined' fullWidth color='secondary' />
-            </Grid>
-            <Grid item xs={12}>
-                <Button variant="text" fullWidth className={classes.btn} sx={{ color: 'purple.text'}}>Criar contato</Button>
-            </Grid>
-        </Grid>
-    </FormControl>
+    </form>
   )
 }
 
