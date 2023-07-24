c8070e3584d3074469d866f57a0d5b194247e7ca
Matheus Araripe <matheus.alc2001@gmail.com>
2023-07-24 12:26:04 -0300

    feat: adição e estilização do botão do formulário

--- src/components/form/ContactForm.jsx
+++ src/components/form/ContactForm.jsx
@@ -1,13 +1,24 @@
-import { TextField, Radio, RadioGroup, Grid, FormControlLabel, FormControl, FormLabel} from '@mui/material'
+import { TextField, Radio, RadioGroup, Grid, FormControlLabel, FormControl, FormLabel, Button} from '@mui/material'
+import { makeStyles } from '@mui/styles';
 import { DatePicker } from '@mui/x-date-pickers';
 import MaleIcon from '@mui/icons-material/Male';
 import FemaleIcon from '@mui/icons-material/Female';
 import React from 'react'
 
+const useStyles = makeStyles({
+    btn:{
+        '&:hover':{
+            backgroundColor: '#484D98'
+        }
+    }
+});
+
 function ContactForm() {
+  
+  const classes = useStyles()
   return (
-    <FormControl sx={{backgroundColor: 'red'}} fullWidth>
-        <Grid container bgcolor={'purple.main'} mt={12} rowSpacing={2} color='purple.text' columnSpacing={1}>
+    <FormControl fullWidth>
+        <Grid container bgcolor={'purple.main'} mt={12} rowSpacing={2} color='purple.text' columnSpacing={1} borderRadius={2} pb={2}>
             <Grid xs={12} sm={12} item>
                 <TextField id="name" label="Nome completo" variant="outlined" fullWidth color="secondary"/>
             </Grid>
@@ -37,6 +48,9 @@ function ContactForm() {
             </Grid>
             <Grid item xs={12} sm={12}>
                 <TextField id='adresses' label='Endereço' variant='outlined' fullWidth color='secondary' />
+            </Grid>
+            <Grid item xs={12}>
+                <Button variant="contained" fullWidth className={classes.btn} sx={{backgroundColor: 'purple.dark', color: 'purple.main'}}>Adicionar contato</Button>
             </Grid>
         </Grid>
     </FormControl>
--- src/themes/index.js
+++ src/themes/index.js
@@ -24,12 +24,4 @@ export const Theme = createTheme({
         }
     },
 
-    // breakpoints: {
-    //     values: {
-    //       mobile: 0,
-    //       tablet: 640,
-    //       laptop: 1024,
-    //       desktop: 1200,
-    //     },
-    // },
 });
