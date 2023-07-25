96fddd4f82542e9428a3e2b736afc20e120049ce
Matheus Araripe <matheus.alc2001@gmail.com>
2023-07-25 17:54:49 -0300

    feat: lógica de adicionar contatos no local storage criada

--- src/context/ContactsContext.jsx
+++ src/context/ContactsContext.jsx
@@ -1,12 +1,34 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 export const ContactsContext = createContext();
 
 export const ContactsProvider = ({children}) => {
 
+    const getContactsFromLs = () => {
+        const data = localStorage.getItem("contacts")
+        if (data){
+            return JSON.parse(data)
+        }
+        return [];
+    };
     
-    
+    const [contacts, setContacts] = useState(getContactsFromLs);
+
+
+    // guarda contatos no localStorage
+    const addUser = (name) =>{
+
+        const newContact = {
+            id: uuidv4(),
+            name,
+        }
+
+        contacts.push(newContact)
+        localStorage.setItem('contacts',JSON.stringify(contacts));
+    };
+
+
     return <ContactsContext.Provider>
         {children}
     </ContactsContext.Provider>
