005609fd4f94e7a2589da7a19090b1824ddda7aa
Matheus Araripe <matheus.alc2001@gmail.com>
2023-07-27 17:22:34 -0300

    fix: mudano o gerenciamento de formulários do formik para react-hook-form

--- src/components/form/ContactForm.jsx
+++ src/components/form/ContactForm.jsx
@@ -1,6 +1,7 @@
 import { TextField, Radio, RadioGroup, Grid, FormControlLabel, FormControl, FormLabel, Button, FormHelperText} from '@mui/material'
-import { useFormik } from 'formik';
 import * as yup from 'yup';
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
 import { ContactsContext } from '../../context/ContactsContext';
 import { makeStyles } from '@mui/styles';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
@@ -19,78 +20,75 @@ const useStyles = makeStyles({
     }
 });
 
+const validationSchema = yup.object().shape({
+    name: yup.string().required('O nome é obrigatório'),
+    cpf: yup.string().required('O CPF é obrigatório').min(11, 'CPF incompleto').max(11,'Formato inválido'),
+    address: yup.string().required('Endereço é obrigatório'),
+    //date: yup.date().required('Data obrigatória'),
+  
+});
+
 function ContactForm() {
 
-  const validationSchema = yup.object().shape({
-        name: yup.string().required('O nome é obrigatório'),
-        cpf: yup.string().required('O CPF é obrigatório').min(11, 'CPF incompleto').max(11,'Formato inválido'),
-        address: yup.string().required('Endereço é obrigatório'),
-        date: yup.date().required('Data obrigatória'),
-      
+  const { control, handleSubmit, reset, formState: { errors } } = useForm({
+      resolver: yupResolver(validationSchema)
     });
 
 
   const {addContact} = useContext(ContactsContext);
 
-  // Configurar o useFormik com a resolução do Yup
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      cpf: '',
-      address: '',
-      gender: 'female',
-      date: null,
-    },
-    validationSchema,
-    onSubmit: (values) => {
-      // Formate a data para o formato "DD/MM/YYYY"
-      const formatDate = values.date? format(new Date(values.date), 'dd/MM/yyyy') : '';
-
-      addContact(values.name, values.cpf, values.address, formatDate, values.gender)
-      console.log(values);
-    },
-  });
+  const dataSubmit = (data) => {
+    //const formatDate = format(new Date(values.date), 'dd/MM/yyyy');
+    console.log('clicado!!');
+    console.log(data);
+    reset();
+  } 
   
-  const classes = useStyles()
+  const classes = useStyles();
+
   return (
-    <form onSubmit={formik.handleSubmit}>
+    <form onSubmit={handleSubmit(dataSubmit)}>
             <Grid container rowSpacing={4} color='purple.text' backgroundColor="purple.main" p={2} justifyContent={'center'} borderRadius={'8px'}>
                 <h2>Adicione um novo contato</h2>
                 <Grid xs={12} sm={12} item>
-                    <FormControl fullWidth error={formik.touched.name && Boolean(formik.errors.name)}>
-                        <TextField 
-                        name='name' 
+                    <Controller
+                    name="name"
+                    control={control}
+                    defaultValue=''
+                    render={({field}) => (
+                        <TextField
+                        {...field}
+                        name='name'
                         label='Nome completo' 
                         variant="outlined" 
-                        fullWidth 
-                        color="secondary"
-                        value={formik.values.name}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
+                        fullWidth
+                        error={!!errors.name}
+                        helperText={errors.name?.message}
                         />
-                        {formik.touched.name && <FormHelperText>{formik.errors.name}</FormHelperText>}
-                    </FormControl>
+                    )}
+                    />
                 </Grid>
                 <Grid item xs={12}>
-                    <FormControl fullWidth error={formik.touched.cpf && Boolean(formik.errors.cpf)}>
-                        <TextField 
-                        name="cpf" 
-                        label="CPF" 
-                        variant="outlined" 
-                        fullWidth 
-                        color="secondary"
-                        value={formik.values.cpf}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        />
-                        {formik.touched.cpf && <FormHelperText>{formik.errors.cpf}</FormHelperText>}
-                    </FormControl>
+                    <Controller
+                        name="cpf"
+                        control={control}
+                        defaultValue=""
+                        render={({field}) => (
+                            <TextField
+                            {...field}
+                            label="CPF" 
+                            variant="outlined" 
+                            fullWidth 
+                            error={!!errors.cpf}
+                            helperText={errors.cpf?.message}
+                            />
+                        )}/>
                 </Grid>
-                <Grid item xs={12}  >
+                {/* <Grid item xs={12}  >
                     <FormControl fullWidth error={formik.touched.cpf && Boolean(formik.errors.cpf)}>
                         <LocalizationProvider dateAdapter={AdapterDayjs}>
                             <DatePicker
-                            value={formik.values.date}
+                            value={null}
                             onChange={(value) => formik.setFieldValue('date', value)}
                             onBlur={formik.handleBlur}
                             label="Data de nascimento" 
@@ -99,46 +97,53 @@ function ContactForm() {
                             {formik.touched.date && <FormHelperText>{formik.errors.date}</FormHelperText>}
                         </LocalizationProvider>
                     </FormControl>
-                </Grid>
+                </Grid> */}
                 <Grid xs={12}  item display={'flex'}  justifyContent={'start'} alignItems={'center'}>
-                    <FormControl fullWidth>
-                        <FormLabel id="genderLable" sx={{color: 'purple.text', marginRight: '3px'}} color='secondary'>Sexo</FormLabel>
+                    <FormLabel id="genderLable" sx={{color: 'purple.text', marginRight: '3px'}} color='secondary'>Sexo</FormLabel>
+                    <Controller
+                    name="gender"
+                    control={control}
+                    defaultValue="Feminino"
+                    render={({field}) => (
                         <RadioGroup
-                        defaultValue={formik.values.gender}
+                        {...field}
+                        defaultValue='Feminino'
                         fullWidth
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        name="gender"
                         row
+                        error={!!errors.gender}
+                        helperText={errors.gender?.message}
                         >
                             <FormControlLabel 
-                            value="female"
+                            value="Feminino"
                             control={<Radio disableRipple icon={<FemaleIcon sx={{color: 'purple.text'}}/>} checkedIcon={<FemaleIcon sx={{color: 'purple.dark'}}/>}/>}
                             label="Feminino" />
                             <FormControlLabel 
-                            value="male" 
+                            value="Masculino" 
                             control={<Radio disableRipple icon={<MaleIcon sx={{color: 'purple.text'}}/>} checkedIcon={<MaleIcon sx={{color: 'purple.dark'}}/>}/>} 
                             label="Masculino" />
                         </RadioGroup>
-                    </FormControl>
+                    )}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={12}>
-                    <FormControl fullWidth error={formik.touched.address && Boolean(formik.errors.address)}>
-                        <TextField 
-                        name='address' 
+                    <Controller
+                    name="address"
+                    control={control}
+                    defaultValue=""
+                    render={({field}) => (
+                        <TextField
+                        {...field}
                         label='Endereço' 
-                        variant='outlined' 
-                        fullWidth 
-                        color='secondary'
-                        value={formik.values.address}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
+                        variant='outlined'
+                        fullWidth
+                        error={!!errors.address}
+                        helperText={errors.address?.message}
                         />
-                        {formik.touched.address && <FormHelperText>{formik.errors.address}</FormHelperText>}
-                    </FormControl>
+                    )}
+                    />
                 </Grid>
                 <Grid item xs={12}>
-                    <Button variant="text" type="submit" fullWidth className={classes.btn} sx={{ color: 'purple.text'}}>Criar contato</Button>
+                    <Button type="submit" variant="text" fullWidth className={classes.btn} sx={{ color: 'purple.text'}}>Criar contato</Button>
                 </Grid>
             </Grid>
     </form>
