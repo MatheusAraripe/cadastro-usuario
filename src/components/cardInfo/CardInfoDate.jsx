763c064c735c114e09a6be9546140450fa25434c
Matheus Araripe <matheus.alc2001@gmail.com>
2023-08-07 12:55:44 -0300

    feat: imagem adicionada a home page

--- src/components/cardInfo/CardInfoDate.jsx
+++ src/components/cardInfo/CardInfoDate.jsx
@@ -6,7 +6,7 @@ function CardInfoDate() {
     <Grid 
     container
     >
-        <Grid item xs={12} sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}} marginTop={4} >
+        <Grid item xs={12} sx={{display: 'flex', justifyContent: 'left', alignItems: 'center'}} marginTop={4} >
             <Avatar src='src\assets\femaleAvatar.jpg' alt="Male Avatar"  sx={{boxShadow: '6', marginRight: '1rem'}} />
             <p>Maria Lúcia faz 32 anos dia 27/09/2023</p>
         </Grid>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { ContactsContext } from '../../context/ContactsContext'
 import { MainContainer, CardInfo, CardInfoDate } from '../../components'
 import { Grid } from '@mui/material'
+import "./home.css"
 
 function Home() {
 
@@ -11,8 +12,14 @@ function Home() {
   return (
     <MainContainer title={"Home"}>
       <Grid container rowSpacing={3} display={'flex'} justifyContent={'center'} alignItems={'center'}>
-        <CardInfo info={contacts}/>
-        <CardInfoDate />
+        <Grid item xs={12} md={6} >
+          <img src="src\assets\hero-image.png" alt="Imagem da tela inicial" />
+        </Grid>
+        <Grid item xs={12} md={6} display={'flex'} justifyContent={'center'} alignItems={'left'} flexDirection={'column'} marginBottom={6}>
+          <CardInfo info={contacts}/>
+          <CardInfoDate />
+        </Grid>
+     
       </Grid>
     </MainContainer>
   )
