b1ed467a4a0df864dbc26be5994e9842f1662b4f
Matheus Araripe <matheus.alc2001@gmail.com>
2023-08-14 15:44:02 -0300

    fix: ajustando responsividade da home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,15 +13,15 @@ function Home() {
     <MainContainer title={"Home"}>
       <Grid container rowSpacing={3} display={'flex'} justifyContent={'center'} alignItems={'center'}>
         <Hidden mdDown>
-          <Grid item xs={12} md={6} >
+          <Grid item xs={12} md={8} lg={7} xl={6}>
             <img src="src\assets\hero-image.png" alt="Imagem da tela inicial" />
           </Grid>
         </Hidden>
-        <Grid item xs={12} md={6} display={'flex'} justifyContent={'center'} alignItems={'left'} flexDirection={'column'} marginBottom={6}>
+        <Grid item xs={12} md={4} lg={5} xl={6} display={'flex'} justifyContent={'center'} alignItems={'left'} flexDirection={'column'} marginBottom={6}>
           <CardInfo info={contacts}/>
           <CardInfoDate />
         </Grid>
-     
+
       </Grid>
     </MainContainer>
   )
