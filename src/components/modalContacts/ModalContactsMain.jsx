94b9a4f50705c9f5ee57d8307f170caa5af91ed3
Matheus Araripe <matheus.alc2001@gmail.com>
2023-08-22 15:20:20 -0300

    fix: ajustando espaço entre colunas no modal

--- src/components/modalContacts/ModalContactsMain.jsx
+++ src/components/modalContacts/ModalContactsMain.jsx
@@ -4,7 +4,7 @@ import { Box, Grid } from '@mui/material'
 function ModalContactsMain({children}) {
   return (
     <Box sx={{backgroundColor: 'white', borderRadius: '8px', padding: '1rem', width: {xs: '300px', md: '400px', lg: '500px'}}} marginY={12} boxShadow={2}>
-        <Grid container rowSpacing={3} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+        <Grid container rowSpacing={3} columnSpacing={1} display={'flex'} justifyContent={'center'} alignItems={'center'}>
             {children}
         </Grid>
     </Box>
