14afa454342096584deeb4e80d35dcf468391eb0
Matheus Araripe <matheus.alc2001@gmail.com>
2023-08-24 15:55:14 -0300

    fix: mudança na maneira de import para o heroku

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-const express = require('express'); // Importar o módulo 'express' corretamente
+import express from 'express';
+import path from 'path';
 
 const app = express();
 
-const path = require('path'); // Importar o módulo 'path' para manipulação de caminhos
+const distPath = path.resolve(__dirname, "./dist");
 
-const distPath = path.resolve(__dirname, "./dist"); // Caminho absoluto para a pasta "./dist"
-
-app.use('/', express.static(distPath)); // Servir arquivos estáticos a partir do caminho distPath
-
-app.use('/contacts', express.static(distPath)); // Servir arquivos estáticos a partir do mesmo caminho para a rota '/contacts'
+app.use('/', express.static(distPath));
+app.use('/contacts', express.static(distPath));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
     if (err) {
         return console.log(err);
     }
-    console.log("App is running!"); // Corrigir o log de sucesso
+    console.log("App is running!");
 });
