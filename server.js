2f5c5036aef936b4ebd5fb10a66b816f512a9175
Matheus Araripe <matheus.alc2001@gmail.com>
2023-08-24 15:50:28 -0300

    fix: mudança na maneira de usar o static

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
-import express from 'express';
+const express = require('express'); // Importar o módulo 'express' corretamente
 
 const app = express();
 
+const path = require('path'); // Importar o módulo 'path' para manipulação de caminhos
 
-app.use('/', express.static(
-    express.resolve(
-        __dirname,
-        "./dist"
-    )
-))
+const distPath = path.resolve(__dirname, "./dist"); // Caminho absoluto para a pasta "./dist"
 
-app.use('/contacts', express.static(
-    express.resolve(
-        __dirname,
-        "./dist"
-    )
-))
+app.use('/', express.static(distPath)); // Servir arquivos estáticos a partir do caminho distPath
 
+app.use('/contacts', express.static(distPath)); // Servir arquivos estáticos a partir do mesmo caminho para a rota '/contacts'
 
-app.listen(process.env.PORT || 3000, (err) => {
-    if(err){return console.log(err)}
-
-    console.log("App is runing!");
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("App is running!"); // Corrigir o log de sucesso
+});
