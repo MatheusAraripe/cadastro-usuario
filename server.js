565cb0585e57cb9b458e8e147b04ffa4c15d081b
Matheus Araripe <matheus.alc2001@gmail.com>
2023-08-24 15:11:25 -0300

    feat: ajustando o deploy para utlizar node js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use('/', express.static(
+    resolve(
+        __dirname,
+        "./dist"
+    )
+))
+
+app.use('/contatos', express.static(
+    resolve(
+        __dirname,
+        "./dist"
+    )
+))
+
+
+app.listen(process.env.PORT || 3000, (err) => {
+    if(err){return console.log(err)}
+
+    console.log("App is runing!");
+})
